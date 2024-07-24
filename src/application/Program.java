fde1e2691a4c8701aa4d5ac42e00a3df56f2997d
Bianka Costa <biankacostads@gmail.com>
2024-07-23 22:32:13 -0300

    Iniciate program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,34 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String path = "D:\\DOCUMENTOS\\summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				String[] product_info = line.split(",");
+				
+				for (String info : product_info) {
+					System.out.println(info);
+				}
+				
+				line = br.readLine();
+			}
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	
+		
 
 	}
 
